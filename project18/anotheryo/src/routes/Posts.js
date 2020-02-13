dd5c26550dafe224261a13d7337e8df372d8cbd9
DevNeoLee <devneolee@gmail.com>
2020-02-13 00:18:22 -0800

    End project18

--- project18/anotheryo/src/routes/Posts.js
+++ project18/anotheryo/src/routes/Posts.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {Route, Link} from 'react-router-dom';
 
+function Post({ match }) {
+    return <div style={ {color: 'red'}}>{ match.params.title}입니다!</div>;
+}
+
 function Posts() {
     return (
       <div>
-        포스트
-        <Link to="/posts/react">react</Link>
+        포스트 입니다.<br />
+        <Link to="/posts/엉호홍">react</Link>
         <br />
-        <Link to="/posts/react">redux</Link>
+        <Link to="/posts/뭐라공">redux</Link>
         <br />
-        <Link to="/posts/react">html</Link>
+        <Link to="/posts/고동도옹">html</Link>
         <br />
-        <Link to="/posts/react">css</Link>
+        <Link to="/posts/g도리">css</Link>
         <br />
+        <Route path="/posts/:title" component={Post} />
       </div>
     );
 }
