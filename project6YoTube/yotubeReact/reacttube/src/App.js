eee148dd25382ba48fd2912da30e03cda738b5f4
DevNeoLee <devneolee@gmail.com>
2020-02-28 22:02:44 -0800

    update project6

--- project6YoTube/yotubeReact/reacttube/src/App.js
+++ project6YoTube/yotubeReact/reacttube/src/App.js
@@ -29,7 +29,7 @@ export default class App extends Component {
     const response = await yotube.get("search", {
       params: {
         part: "snippet",
-        maxResults: 11,
+        maxResults: 36,
         key: `AIzaSyCeCFeVFIlSPjwDclJxBKMTuVfnH43nCgQ`,
         q: searchTerm
       }
